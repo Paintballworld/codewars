1f2068ade3f75344332dee37e1992c80f444d41d
PaintballWorld <PaintballWorld@rambler.ru>
2017-08-16 20:27:56 +0600

    one more

--- src/WrongDouble.java
+++ src/WrongDouble.java
@@ -1,10 +1,27 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Created by me on 16.08.17.
  */
 public class WrongDouble {
 
-    public static double findUniq(double arr[]) {
+    public static double findUnique(double arr[]) {
         // Do the magic
-        return arr[0];
+        return Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey))
+                .get(1L);
+    }
+
+    private static double precision = 0.0000000000001;
+
+    public static void main(String[] args) {
+        System.out.println(findUnique(new double[]{0, 1, 0})); // 1.0
+        System.out.println(findUnique(new double[]{1, 1, 1, 2, 1, 1})); // 2.0
     }
 }
